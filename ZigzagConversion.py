8968f9f2671c32958d148ae1161efde4eb73dd65
Insoftex-CODE <149436490+Insoftex-CODE@users.noreply.github.com>
2023-10-30 22:54:01 -0500

    fixed code
    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    
    P   A   H   N
    A P L S I I G
    Y   I   R
    And then read line by line: "PAHNAPLSIIGYIR"
    
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string s, int numRows);
     
    
    Example 1:
    
    Input: s = "PAYPALISHIRING", numRows = 3
    Output: "PAHNAPLSIIGYIR"
    Example 2:
    
    Input: s = "PAYPALISHIRING", numRows = 4
    Output: "PINALSIGYAHRPI"
    Explanation:
    P     I    N
    A   L S  I G
    Y A   H R
    P     I
    Example 3:
    
    Input: s = "A", numRows = 1
    Output: "A"
     
    
    Constraints:
    
    1 <= s.length <= 1000
    s consists of English letters (lower-case and upper-case), ',' and '.'.
    1 <= numRows <= 1000

--- ZigzagConversion.py
+++ ZigzagConversion.py
@@ -1,35 +1,30 @@
-def findMedianSortedArrays(nums1, nums2):
-    if len(nums1) > len(nums2):
-        nums1, nums2 = nums2, nums1
+def convert(s, numRows):
+    if numRows == 1 or numRows >= len(s):
+        return s
 
-    x, y = len(nums1), len(nums2)
-    low, high = 0, x
+    rows = [''] * numRows
+    index, step = 0, 1
 
-    while low <= high:
-        partitionX = (low + high) // 2
-        partitionY = (x + y + 1) // 2 - partitionX
+    for char in s:
+        rows[index] += char
+        if index == 0:
+            step = 1
+        elif index == numRows - 1:
+            step = -1
+        index += step
 
-        maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-        maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+    return ''.join(rows)
 
-        minX = float('inf') if partitionX == x else nums1[partitionX]
-        minY = float('inf') if partitionY == y else nums2[partitionY]
-
-        if maxX <= minY and maxY <= minX:
-            if (x + y) % 2 == 0:
-                return (max(maxX, maxY) + min(minX, minY)) / 2
-            else:
-                return max(maxX, maxY)
-        elif maxX > minY:
-            high = partitionX - 1
-        else:
-            low = partitionX + 1
 
 # Example usage:
-nums1 = [1, 3]
-nums2 = [2]
-print(findMedianSortedArrays(nums1, nums2)) # Output: 2.00000
+s1 = "PAYPALISHIRING"
+numRows1 = 3
+print(convert(s1, numRows1))  # Output: "PAHNAPLSIIGYIR"
 
-nums1 = [1, 2]
-nums2 = [3, 4]
-print(findMedianSortedArrays(nums1, nums2)) # Output: 2.50000
+s2 = "PAYPALISHIRING"
+numRows2 = 4
+print(convert(s2, numRows2))  # Output: "PINALSIGYAHRPI"
+
+s3 = "A"
+numRows3 = 1
+print(convert(s3, numRows3))  # Output: "A"
